5154a0c6b3f57df6329f761aab07b96eabed51e6
Yahia <y.ferchouli@esi-sba.dz>
2024-10-27 14:15:07 +0100

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project aims to predict whether a video title represents a comedic chronicle.',
+    author='Yahia FERCHOULI',
+    license='MIT',
+)
